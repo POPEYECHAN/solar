4f3bb8187e45c229725a05f615520e045932b5af
POPEYECHAN <newkkk@126.com>
2019-01-24 10:35:59 +0800

    ClassLoad测试
    ClassLoad测试

---
+++ classload/src/test/java/com/solar/load/AppTest.java
@@ -0,0 +1,27 @@
+package com.solar.load;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("00");
+        assertTrue( true );
+    }
+
+    public void test001() {
+        ClassLoader.getSystemClassLoader();
+    }
+
+
+}
